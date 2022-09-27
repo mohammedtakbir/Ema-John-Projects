ea60046b619d11e8b3e9a16d811a08141dc13d08
mohammedtakbir <takbirhossain678@gmail.com>
2022-09-27 20:58:06 +0600

    second commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
-const Cart = ({cart}) => {
+import './Cart.css'
+const Cart = (props) => {
+    const {cart} = props;
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for(const product of cart){
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+    // const tax = total * 10/100;
+    const tax = (total * 0.1).toFixed(2);
+    const grandTotal = total + shipping + parseFloat(tax);
     return (
-        <div>
+        <div className='cart'>
             <h4>this is order summery</h4>
-            <h3>Selected item: {cart.length}</h3>
+            <p>Selected item: {quantity}</p>
+            <p>Total price: ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Total Tax: ${tax}</p>
+            <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,37 +3,63 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
-import './Shop.css'
+import './Shop.css';
+import { addToDb, getStoredCart } from '../../utilities/fakedb'
 const Shop = () => {
     //? declare a state for products
     const [products, setProducts] = useState([]);
-
     //? lift up the state--> product component e state declare na kore 1 level opore uthaiya shop component e state declare korse. jate cart-container e state er value access kora jay
     const [cart, setCart] = useState([]);
 
     //? load data from public folder
-    useEffect( () => {
+    useEffect(() => {
         fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data))
+            .then(res => res.json())
+            .then(data => setProducts(data))
     }, []);
 
-    const handleAddToCart = (product) => {
-        console.log(product);
-        //! do not do this cart.push(product)
-        const newCart = [...cart, product];
+    //? jekhan thika cart er data update kora hoitase, seikhan thikai data load korte hobe
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id]
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart); 
+        //? products er opor depend kore useEffect execute hobe
+        //? products er data chenge hole useEffectr execute hobe
+    }, [products])
+
+    const handleAddToCart = (selectedProduct) => {
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        let newCart = [];
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists]
+        }
+        //! do not use cart.push(product)
         setCart(newCart);
+        addToDb(selectedProduct.id);
     };
 
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product => <Product 
-                        product={product} 
+                    products.map(product => <Product
+                        product={product}
                         key={product.id}
                         handleAddToCart={handleAddToCart}
-                        ></Product>)
+                    ></Product>)
                 }
             </div>
             <div className="cart-container">
